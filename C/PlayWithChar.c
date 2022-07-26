972f8f789d7906b0f5392bd9ee239affc6852415
Hari Vigneshwaran <96340194+coderhariexpert@users.noreply.github.com>
2022-07-26 10:10:27 +0530

    Initial

---
+++ C/PlayWithChar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{ 
+    char ch;
+    char s[20];
+    char sen[100];
+    scanf("%c", &ch);
+    scanf("%s", &s);
+    scanf("\n");
+    scanf("%[^\n]%*c", &sen);
+    printf("%c\n", ch);
+    printf("%s\n", s);
+    printf("%s\n", sen);    
+    return 0;
+}
